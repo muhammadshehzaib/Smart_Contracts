d1a6cf8bfc2a26adbadbda9f4a83a44cb500d49c
m-Shehzaib <98905825+muhammadshehzaib@users.noreply.github.com>
2024-04-04 15:05:01 +0500

    Deploying on the sepolia TESTNET

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.TESTNET_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
   solidity: "0.8.24",
 };
